a391e2448e22e94e187b5653afef75b54b49e3e8
Michael Musick <m.a.musick@gmail.com>
2016-08-31 13:48:29 -0600

    Added in class example 1-1 to repository

---
+++ 340/classEx/week-1-1/sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(1000, 400);
+  background(255, 0, 0);
+}
+
+function draw() {
+	
+	// This is a rectangle
+	stroke(0);
+	fill(100, 100, 0);
+  rect(50, 100, 100, 70);
+	
+	// This is a traingle
+	// noStroke();
+	// fill(0, 0, 200, 98);
+	// triangle(50, 50, 100, 100, 50, 200);
+	
+	// this is a circle
+	// ellipseMode(CORNER);
+	fill(50, 0, 200, 98);
+	ellipse(50, 100, 100, 100);
+	
+	fill(200, 100, 100, 99);
+	ellipse(50, 100, 50, 10);
+	
+}
